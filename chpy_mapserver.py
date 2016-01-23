b7a9682bd36d4e485ef0331b0c87ea6fcc6e551a
benblass <b.blassel@gmail.com>
2016-01-23 19:40:42 +0100

    Fixed the CORS issue

--- chpy_mapserver.py
+++ chpy_mapserver.py
@@ -1,43 +1,54 @@
 import cherrypy
 import time
 import os, os.path
-from jinja2 import Environment, FileSystemLoader
+import simplejson
 
 host_ip = '0.0.0.0'
 host_port = 8081
 
 local_path = os.path.dirname(os.path.abspath(__file__))
 
+configFile = "map.config"
+
 cherrypy_conf = {
 	'/': {
 		'tools.sessions.on': False,
 		'tools.staticdir.root': local_path
-		}
+	},
+	'/static': {
+		'tools.staticdir.on': True,
+		'tools.staticdir.dir': './static'
 	}
+}
 
 class MapServer(object):
-    def __init__(self):
-        self.maptiles = Map()
+    def __init__(self, mapconfig={}):
+     self.displayoptions = mapconfig['displayoptions']
+     self.area = mapconfig['name']
+     self.areapath = mapconfig['path']
+     pass
 
     @cherrypy.expose
-    def index(self):
-        return 'Running tout court'
+    @cherrypy.tools.json_out()
+    def get_map_params(self):
+        return dict(area=self.area, areapath=self.areapath, displayoptions = self.displayoptions)
 
-@cherrypy.popargs('fz','z','x','y')
-class Map(object):
-    @cherrypy.expose
-    def index(self, fz, z,x,y):
-        return '%s/%s/%s/%s' % (fz,z,x,y)
+def loadMapConfig(filename=''):
+	json_data = open(filename).read()
+	return simplejson.loads(json_data)
 
+def CORS():
+	cherrypy.response.headers["Access-Control-Allow-Origin"] = "*"
 
-mapserver = MapServer();
+mapserver = MapServer(loadMapConfig(configFile));
 
 cherrypy.tree.mount(mapserver, '/', config = cherrypy_conf)
 
 cherrypy.config.update({
 	'server.socket_port': host_port,
-	'server.socket_host': host_ip
+	'server.socket_host': host_ip,
+	'tools.CORS.on': True
 	})
-
+cherrypy.tools.CORS = cherrypy.Tool('before_handler', CORS)
 cherrypy.engine.start()
 cherrypy.engine.block()
